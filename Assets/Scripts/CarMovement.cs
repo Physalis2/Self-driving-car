02df60a9e10297ccde9d54b1908ad216f4e18f93
Physalis <143352299+Physalis2@users.noreply.github.com>
2024-01-19 23:50:08 +0100

    some git ignore updates

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -58,7 +58,6 @@ public class CarMovement : MonoBehaviour
             net.setFitness(totalDistance);
         }
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("colission");
